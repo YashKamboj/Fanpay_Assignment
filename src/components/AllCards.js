7c4280d1380fb358d32f9af824d8c5da00f13546
YashKamboj <yashkamboj29@gmail.com>
2022-08-13 23:55:48 +0530

    added HC9 Card
    Signed-off-by: YashKamboj <yashkamboj29@gmail.com>

--- src/components/AllCards.js
+++ src/components/AllCards.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
+import DisplayHC1Cards from "./HC1Card";
+import HC3Card from "./HC3Card";
 import HC5Card from "./HC5Card";
 import HC6Card from "./HC6Card";
-import HC1Card from "./HC1Card";
 import HC9Card from "./HC9Card";
-import HC3Card from "./HC3Card";
 import FetchedData from "../FetchedData";
 
 const AllCards = () => {
+    const [HC1Data, setHC1Data] = useState();
     const [HC5Data, setHC5Data] = useState();
     const [HC6Data, setHC6Data] = useState();
     const [HC9Data, setHC9Data] = useState();
 
     useEffect(() => {
         FetchedData().then((data) => {
+          setHC1Data(data.HC1Cards);
           setHC5Data(data.HC5Cards);
           setHC6Data(data.HC6Cards);
           setHC9Data(data.HC9Cards);
@@ -26,7 +28,7 @@ const AllCards = () => {
         <HC5Card data={HC5Data} />
         <HC6Card data={HC6Data}  />
         <HC9Card data={HC9Data}  />
-        <HC1Card  />
+        <DisplayHC1Cards data={HC1Data}  />
           </>
     )
 };
---
+++ src/components/HC1Card/HC1Card.style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const HC1CardWrapper = styled.a`
+    text-decoration: none;
+    color: black;
+    cursor: pointor;
+
+  .HC1CardNoArrow {
+    display: flex;
+    align-items: center;
+    margin: 0 1rem 0 0;
+    background-color: #fbaf03;
+    border-radius: 12px;
+    width : 70vw;    
+    
+    div{
+    display: flex;
+    flex-direction: column;
+
+    b{
+      padding: 1rem 0 0 1.2rem;
+    font-size: 1rem;
+    }
+    p{
+      padding: 0px 1rem 1rem 1rem;
+      margin: 0 0 0 0.3rem;
+      font-size: 0.8rem;
+      color: rgba(27, 27, 30, 0.725524);
+    }
+  }
+    img{
+      padding-left: 1rem;
+      height: 2rem;
+    }
+  }
+  .nonScrollable {
+    width: 100%;
+  }
+`;
+
+export const HC1CardsWrapper = styled.div`
+.HC1Cards{
+  display: flex;
+  padding: 1rem;
+  gap: 1rem;
+}
+.NoArrowScrollable {
+  overflow: auto;
+}
+`;
